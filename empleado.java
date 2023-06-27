748f4aa9a17e4578001c64129ea42344d0f0fc10
GianlucaZeolla <134320533+GianlucaZeolla@users.noreply.github.com>
2023-06-27 11:01:04 -0300

    empleado.java

--- empleado.java
+++ empleado.java
@@ -1,41 +1,19 @@
-public class Empleado {
-        private String nombre;
-        private double[] salario;
-        private int antiguedad;
+public class EmpleadoTest {
+    public static void main(String[] args) {
 
-        public Empleado(String nombre, double[] salario, int antiguedad) {
-            this.nombre = nombre;
-            this.salario = salario;
-            this.antiguedad = antiguedad;
-        }
-        public String getNombre() {
-            return nombre;
-        }
-        public void setNombre(String nombre) {
-            this.nombre = nombre;
-        }
-        public double[] getSalario() {
-            return salario;
-        }
-        public void setSalario(double[] salario) {
-            this.salario = salario;
+        Empleado[] empleados = {
+                new Empleado("Jose", new double[]{1000}, 5),
+                new Empleado("Juan", new double[]{15}, 10),
+                new Empleado("Maria", new double[]{0.1}, 4)
+        };
+        for (Empleado empleado : empleados) {
+            empleado.aplicarAumento();
+            System.out.println("-------------------");
+
+            System.out.println("Empleado: " + empleado.getNombre()
+                    + " - Salario Actual: $" + empleado.getSalario());
         }
 
-    public int getAntiguedad() {
-        return antiguedad;
     }
-    public void setAntiguedad(int antiguedad) {
-            this.antiguedad = antiguedad;
-    }
-    @Override
-    public String toString() {
-            return "Empleado{" + "nombre=" + nombre + ", salario=" + salario + ", antiguedad";
-    }
-    public void aplicarAumento(String salarioEntrada){
-            if (antiguedad>5){
-                salario += salario + salarioEntrada;
-            }
-    }
-
 
 }
