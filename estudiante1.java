7a998725b44bca716c0ee1bd5f7ecb69b5eb18f2
GianlucaZeolla <134320533+GianlucaZeolla@users.noreply.github.com>
2023-07-18 15:58:05 -0300

    Create estudiante1.java

---
+++ estudiante1.java
@@ -0,0 +1,44 @@
+public class ejer2 {
+
+	public static void main(String[] args) {
+
+		 Estudiante[] estu = new  Estudiante[3];
+		 estu[0] = new  Estudiante("Ricardo", 20, 9);
+		 estu[1] = new  Estudiante("Roberto", 18, 7);
+		 estu[2] = new  Estudiante("Ramon", 17, 6);
+	      
+		 int calificacionMasAlta = -1; // Inicializamos con un valor negativo para asegurar que cualquier calificación será mayor.
+
+	        for (Estudiante estudiante : estu) {
+	            if (estudiante.getCali() > calificacionMasAlta) {
+	                calificacionMasAlta = estudiante.getCali();
+	            }
+	        }
+
+	        System.out.println("La calificación más alta es: " + calificacionMasAlta);
+	    }
+	
+	    
+	}
+
+	class Estudiante {
+	    private
+	    String nombre;
+	    int edad;
+	    int calificaciones;
+	    public
+	    Estudiante(String n, int e,int c){
+	        nombre = n;
+	        edad = e;
+	        calificaciones = c;
+	    }
+	    String getNombre (){
+	        return nombre;
+	    }
+	    int getEdad() {
+	        return edad;
+	    }
+	    int getCali() {
+	        return calificaciones;
+	    }
+	}*
