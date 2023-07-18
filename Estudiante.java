ae5b299360593312508bf7ec9cb009f016d77eeb
GianlucaZeolla <134320533+GianlucaZeolla@users.noreply.github.com>
2023-07-18 15:35:32 -0300

    Estudiante.java

--- Estudiante.java
+++ Estudiante.java
@@ -1,45 +1,29 @@
-publicpublic class Estudiante {
-
+public class Estudiante {
     private String nombre;
     private int edad;
-    private double[] calificaciones;
+    private int[] calificaciones;
 
-    public Estudiante(String nombre, int edad, double[] calificaciones) {
+    public Estudiante (String nombre, int edad, int[] calificaciones) {
         this.nombre = nombre;
         this.edad = edad;
         this.calificaciones = calificaciones;
     }
-
     public String getNombre() {
         return nombre;
     }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
     public int getEdad() {
         return edad;
     }
-
-    public void setEdad(int edad) {
-        this.edad = edad;
-    }
-
-    public double[] getCalificaciones() {
+    public int[] getCalificaciones() {
         return calificaciones;
     }
-
-    public void setCalificaciones(double[] calificaciones) {
-        this.calificaciones = calificaciones;
-    }
-
-    public double calcularPromedio() {
-        double suma = 0;
-        for (double calificacion : calificaciones) {
+    public double getPromedioCalificaciones() {
+        int suma = 0;
+        for (int calificacion : calificaciones) {
             suma += calificacion;
         }
-        double v = suma / calificaciones.length;
-        return v;
+        return (double) suma / calificaciones.length;
     }
-}
+
+
+    }
